1a49fc35a7b34369431d9d114e3373dc063179f8
RatRatARMY <khang0762412064@outlook.com>
2025-04-05 16:27:26 +0700

    Main file C++ of this project

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <cppheader/0.hpp>
+#include <cppheader/2.hpp>
+#include <iostream>
+int main(int argc, char** argv) {
+    std::cout << "Testing thing for that thing" << std::endl;
+    cppheader::thing();
+    cppheader::DestructingThings::tsarbomba();
+}
